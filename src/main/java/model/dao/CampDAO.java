1402a8c3660b2243457b70a61155045031901085
LAPTOP-E45PMUCF\s22su <yu-sato@seassist.co.jp>
2025-02-12 14:17:24 +0900

    sato0212

--- src/main/java/model/dao/CampDAO.java
+++ src/main/java/model/dao/CampDAO.java
@@ -10,8 +10,8 @@ import model.entity.CampBean;
 public class CampDAO {
 	//キャンプ場で絞り込み
 	public CampBean getCampListByCampName(CampBean campBean) throws ClassNotFoundException, SQLException {
-		//SQL文
-		String sql = "SELECT camp_name,location,tel,charge,capacity FROM camp WHERE camp_name=?";
+		//SQL文（caharge修正必要）
+		String sql = "SELECT camp_name,location,tel,caharge,capacity FROM camp WHERE camp_name=?";
 		//データベースに接続
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pstmt = con.prepareStatement(sql);) {
@@ -23,7 +23,7 @@ public class CampDAO {
 				String campName = res.getString("camp_name");
 				String location = res.getString("location");
 				String tel = res.getString("tel");
-				int charge = res.getInt("charge");
+				int charge = res.getInt("caharge");
 				int capacity = res.getInt("capacity");
 				campBean = new CampBean(campName, location, tel, charge, capacity);
 			}
@@ -34,12 +34,12 @@ public class CampDAO {
 	//編集登録処理
 	public int updateCampList(CampBean campBean) throws ClassNotFoundException, SQLException {
 		int resultNum = 0;
-		//SQL文
+		//SQL文（caharge修正必要）
 		String sql = "UPDATE camp SET";
 		sql += " camp_name=?,";
 		sql += " location=?,";
 		sql += " tel=?,";
-		sql += " charge=?,";
+		sql += " caharge=?,";
 		sql += " capacity=?";
 		sql += " WHERE camp_name=?";
 		//データベースに接続
@@ -57,5 +57,35 @@ public class CampDAO {
 		}
 		return resultNum;
 	}
+	
+	//キャンプ場の新規登録をするメソッド
+		public int RegistCampList(CampBean campBean) throws ClassNotFoundException, SQLException {
+			//SQL
+			String sql = "INSERT INTO camp (camp_name,location,tel,caharge,capacity )VALUES (?, ?, ?, ?, ?)";
+			int resultNum=0;
+			//データベースに接続
+			try (Connection con = ConnectionManager.getConnection();//スロー宣言の追加
+					PreparedStatement pstmt = con.prepareStatement(sql);){
+				//プレースホルダに値を設定
+				pstmt.setString(1, campBean.getCampName());
+				pstmt.setString(2, campBean.getLocation());
+				pstmt.setString(3, campBean.getTel());
+				pstmt.setInt(4, campBean.getCharge());
+				pstmt.setInt(5, campBean.getCapacity());
+				pstmt.setString(6, campBean.getCampName());
+				
+				//SQLの実行
+				resultNum = pstmt.executeUpdate();
+				System.out.println(campBean.getCampName());
+			}
+			return resultNum;
+		}
+	
+	
+	
+	
+	
+	
+	
 
 }
